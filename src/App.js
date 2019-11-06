babbaab22b693697261dc703fe4e9111802d3ce5
Ricardo Sousa <ricardo.dmsousa@gmail.com>
2019-11-06 20:11:35 -0300

    Listando contatos de um array, criando um componente e pegando esse array de um outro arquivo

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
 
-class ContactList extends React.Component{
-  render(){
-    const people = this.props.contacts
-    return <ol>
-      {people.map(person =>(
-        <li key={person.name}>{person.name}</li>
-      ))}
-    </ol>
+const contacts = [
+    {
+      "id": "ryan",
+      "name": "Ryan Florence",
+      "email": "ryan@reacttraining.com",
+      "avatarURL": "http://localhost:5001/ryan.jpg"
+    },
+    {
+      "id": "michael",
+      "name": "Michael Jackson",
+      "email": "michael@reacttraining.com",
+      "avatarURL": "http://localhost:5001/michael.jpg"
+    },
+    {
+      "id": "tyler",
+      "name": "Tyler McGinnis",
+      "email": "tyler@reacttraining.com",
+      "avatarURL": "http://localhost:5001/tyler.jpg"
+    }
+  ]
+
+  export default class App extends Component{
+      render(){
+          return(
+              <div>
+                  <ListContacts contacts={contacts}/>
+              </div>
+          )
+      }
   }
-}
-
-function App() {
-  return (
-    <div className="App">
-      <ContactList contacts={
-        [
-          {name:'Michal'},
-          {name:'Ryan'},
-          {name:'Tyler'}
-        ]
-      }/>
-      <ContactList contacts={
-        [
-          {name:'Amanda'},
-          {name:'Richard'},
-          {name:'Jeff'}
-        ]
-      }/>
-    </div>
-  );
-}
-
-export default App;
